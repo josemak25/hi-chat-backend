8b210e97cda3faba92c822d15b145cf6014c28a1
josemak25 <amakirij@gmail.com>
2019-08-19 00:43:04 +0100

    add app start point and complete configuration of database connection

---
+++ api/app.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const compress = require('compression');
+const methodOverride = require('method-override');
+const cors = require('cors');
+const expressWinston = require('express-winston');
+const helmet = require('helmet');
+const mapRoutes = require('express-routes-mapper');
+const winstonInstance = require('../config/winston');
+const routes = require('../routes/index');
+const error = require('../config/errors');
+const auth = require('./policies/auth.policy');
+const { env } = require('../config/env');
+
+const app = express();
+// secure apps by setting various HTTP headers
+app.use(helmet());
+app.use(cors({ credentials: true, origin: true }));
+app.use(cookieParser());
+
+// parse body params and attach them to res.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(compress());
+app.use(methodOverride());
+
+// enable detailed API logging in dev env
+//comment this code to reduce api logs
+if (env === 'development') {
+  app.use(logger('dev'));
+  expressWinston.responseWhitelist.push('body');
+  app.use(
+    expressWinston.logger({
+      winstonInstance,
+      meta: true, // optional: log meta data about request (defaults to true)
+      msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+      colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+    })
+  );
+}
+
+// mount all routes on /api path
+const mappedOpenRoutes = mapRoutes(routes.publicRoutes, 'api/controllers/');
+const mappedAuthRoutes = mapRoutes(routes.privateRoutes, 'api/controllers/');
+
+// secure your private routes with jwt authentication middleware
+app.all('/api/v1/private/*', (req, res, next) => auth(req, res, next));
+
+// fill routes for express application
+app.use('/api/v1/public', mappedOpenRoutes);
+app.use('/api/v1/private', mappedAuthRoutes);
+
+// if error is not an instanceOf APIError, convert it.
+app.use(error.converter);
+
+// catch 404 and forward to error handler
+app.use(error.notFound);
+
+// error handler, send stacktrace only during development
+app.use(error.handler);
+
+if (env !== 'test') {
+  app.use(
+    expressWinston.errorLogger({
+      winstonInstance
+    })
+  );
+}
+
+// error handler, send stacktrace only during development
+app.use(error.handler);
+
+module.exports = app;
---
+++ index.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const http = require('http');
+const util = require('util');
+
+// config should be imported before importing any other file
+const config = require('./config/env');
+const app = require('./api/app');
+
+const server = http.Server(app);
+
+const debug = require('debug')('circa:index');
+
+// make bluebird default Promise
+Promise = require('bluebird'); // eslint-disable-line no-global-assign
+
+// plugin bluebird promise in mongoose
+mongoose.Promise = Promise;
+
+// connect to mongo db
+const mongoUri = config.mongo.host;
+mongoose.connect(mongoUri, {
+  keepAlive: 1,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
+// print mongoose logs in dev env
+if (config.mongooseDebug) {
+  mongoose.set('debug', (collectionName, method, query, doc) => {
+    debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+  });
+}
+
+//opens a port if the envirnoment is not test
+if (process.env.NODE_ENV !== 'test') {
+  // listen on port config.port
+  server.listen(config.port, () => {
+    console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
+  });
+}
+
+module.exports = server;
